3a290ea9b64f6f2ad4e62a3703c504c5f2731c2e
Daniel De La Pena <del17011@byui.edu>
2023-04-03 23:20:45 -0600

    header in jobs page

--- jobsea/src/pages/jobs.js
+++ jobsea/src/pages/jobs.js
@@ -3,15 +3,32 @@ import SearchBar from "../components/searchBar";
 import jobsCSS from "./jobs.module.css";
 import { useState } from "react";
 
+const Header = (props) => {
+  return (
+    <div className={jobsCSS.logonOptions}>
+      <div>Hello Daniel</div>
+      <div>|</div>
+      <div onClick={props.signOut}>Sign out</div>
+    </div>
+  );
+};
+
 const Jobs = () => {
   const [searchQuery, setSearchQuery] = useState();
-  useEffect(() => {
-  }, [searchQuery]);
+
+  useEffect(() => {}, [searchQuery]);
+
+  const signOut = () => {
+    localStorage.removeItem("token");
+  };
+
   const getSearchQuery = (searchInput) => {
     setSearchQuery(searchInput);
   };
+
   return (
     <div className={jobsCSS.jobsCSS}>
+      <Header signOut={signOut} />
       <SearchBar getInput={getSearchQuery} />
     </div>
   );
