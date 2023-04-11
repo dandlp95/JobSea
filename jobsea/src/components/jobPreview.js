b835305b7e17bdcb9f690cc06e95cacadf36774d
Daniel De La Pena <del17011@byui.edu>
2023-04-10 23:24:51 -0600

    progress on expandable job preview

--- jobsea/src/components/jobPreview.js
+++ jobsea/src/components/jobPreview.js
@@ -1,10 +1,11 @@
 import React from "react";
 import jobPreviewCSS from "./jobPreview.module.css";
+import { MdKeyboardArrowDown } from "react-icons/md"
 
 const JobPreview = (props) => {
   return (
     <div className={jobPreviewCSS.jobPreviewCSS}>
-      <div className={jobPreviewCSS.flexContainer}>
+      {/* <div className={jobPreviewCSS.flexContainer}>
         <div>
           Position: <span>{props.job.position}</span>
         </div>
@@ -14,6 +15,24 @@ const JobPreview = (props) => {
         <div>
           Company: <span>{props.job.company}</span>
         </div>
+      </div>
+      <div className={jobPreviewCSS.arrowIcon}><MdKeyboardArrowDown /></div> */}
+      <div className={jobPreviewCSS.previewContainer}>
+        <div className={jobPreviewCSS.flexContainer}>
+          <div>
+            Position: <span>{props.job.position}</span>
+          </div>
+          <div>
+            Status: <span>{props.job.status}</span>
+          </div>
+          <div>
+            Company: <span>{props.job.company}</span>
+          </div>
+        </div>
+        <div className={jobPreviewCSS.arrowIcon}><MdKeyboardArrowDown /></div>
+      </div>
+      <div className={jobPreviewCSS.applicationDetails}>
+        HELLOW WORLD
       </div>
     </div>
   );
