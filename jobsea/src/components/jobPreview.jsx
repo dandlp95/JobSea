7c549290e24b22ce2ff9cfcfd1aa0755c0ef9730
Daniel De La Pena <del17011@byui.edu>
2023-04-25 23:16:15 -0600

    minor stuff

--- jobsea/src/components/jobPreview.jsx
+++ jobsea/src/components/jobPreview.jsx
@@ -19,18 +19,6 @@ const JobPreview = props => {
 
   return (
     <div className={jobPreviewCSS.jobPreviewCSS}>
-      {/* <div className={jobPreviewCSS.flexContainer}>
-        <div>
-          Position: <span>{props.job.position}</span>
-        </div>
-        <div>
-          Status: <span>{props.job.status}</span>
-        </div>
-        <div>
-          Company: <span>{props.job.company}</span>
-        </div>
-      </div>
-      <div className={jobPreviewCSS.arrowIcon}><MdKeyboardArrowDown /></div> */}
       <div
         className={`${jobPreviewCSS.previewContainer} ${
           !isCollapsed ? jobPreviewCSS.flattenBorder : ''
