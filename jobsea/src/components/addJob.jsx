34e88b35a0c48def759a3ca14c21a86f843cf4c0
Daniel De La Pena <del17011@byui.edu>
2023-07-15 01:04:13 -0600

    Added form field to get event date and will show or hide depending on what the client radio option was. Also added button and call to API to create the form. Also added function to delete form input assuming form data is succesfully posted in the database.

--- jobsea/src/components/addJob.jsx
+++ jobsea/src/components/addJob.jsx
@@ -11,6 +11,8 @@ const AddJob = () => {
   const [salary, setSalary] = useState()
   const [location, setLocation] = useState()
   const [statusOptions, setStatusOptions] = useState()
+  const [eventDate, setEventDate] = useState()
+  const [eventDateQuestion, setEventDateQuestion] = useState()
 
   useEffect(() => {
     const getStatusOptions = async () => {
@@ -33,6 +35,7 @@ const AddJob = () => {
 
   const handleRadioOptionChange = event => {
     setSelectedRadioOption(event.target.value)
+    setQuestion(event.target.value)
   }
 
   const handlePositionChange = event => {
@@ -59,23 +62,67 @@ const AddJob = () => {
     setLocation(event.target.value)
   }
 
-  const sendRequest = async () => {
-    // const token = localStorage.getItem <- GET TOKEN FROM LOCAL STORAGE
+  const handlEventDate = event => {
+    setEventDate(event.target.value)
+  }
 
+  const setQuestion = statusId => {
+    const hiredQuestion = 'Congrats! When will your new job start?'
+    const interviewQuestion = 'When will your interview be?'
+    const waitingQuestion = 'When do you estimate you will hear back from them?'
+
+    if (statusId == 1) setEventDateQuestion(hiredQuestion)
+    else if (statusId == 3) setEventDateQuestion(interviewQuestion)
+    else if (statusId == 5) setEventDateQuestion(waitingQuestion)
+    else setEventDateQuestion()
+  }
+
+  const sendRequest = async () => {
+    const token = localStorage.getItem('token')
+    const userId = localStorage.getItem('userId')
     const options = {
       method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      Authorization: `Bearer ${'TOKEN GOES HERE'}`,
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
       body: JSON.stringify({
-        jobTitle: position,
         company: company,
+        jobTitle: position,
         salary: salary,
         location: location,
         link: link,
         comments: comments,
-        jobStatus: selectedRadioOption
+        firstUpdate: {
+          eventDate: eventDate,
+          notes: comments,
+          statusId: selectedRadioOption
+        },
+        userId: userId
       })
     }
+
+    const response = await fetch(
+      'https://localhost:7283' + '/jobsea/JobApplication/CreateApplication',
+      options
+    )
+
+    if (response.ok) {
+      // This is a placeholder
+      alert('Success')
+      clearCreateApplicationForm()
+    }
+  }
+
+  const clearCreateApplicationForm = () => {
+    setSelectedRadioOption()
+    setPosition()
+    setCompany()
+    setLink()
+    setComments()
+    setSalary()
+    setLocation()
+    setEventDate()
   }
 
   return (
@@ -115,10 +162,16 @@ const AddJob = () => {
                 </label>
               </div>
             ))}
+          {eventDateQuestion && (
+            <div>
+              <label for='eventDate'>{eventDateQuestion} </label>
+              <input type='date' name='eventDate' onChange={handlEventDate} />
+            </div>
+          )}
         </div>
         <div>
           <label for='salary'>Salary: </label>
-          <input type='text' name='salary' onChange={handleSalaryChange} />
+          <input type='number' name='salary' onChange={handleSalaryChange} />
         </div>
         <div>
           <label for='location'>Location: </label>
@@ -132,6 +185,7 @@ const AddJob = () => {
           <label for='comment'>Additional notes: </label>
           <input type='text' name='comment' onChange={handleCommentChange} />
         </div>
+        <Button btnText="Create Application" clickAction={sendRequest}/>
       </form>
     </div>
   )
