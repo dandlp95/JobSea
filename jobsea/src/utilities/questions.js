a5839cc29c35fd4a72f192ddc434b5fa2974cd56
Daniel De La Pena <del17011@byui.edu>
2023-08-23 22:39:28 -0600

    enum questions for maintanability, need to implement

---
+++ jobsea/src/utilities/questions.js
@@ -0,0 +1,9 @@
+// need to implement this...
+
+const questions = {
+  hiredQuestion: 'Congrats! When will your new job start?',
+  interviewQuestion: 'When will your interview be?',
+  waitingQuestion: 'When do you estimate you will hear back from them?'
+}
+
+export default questions
