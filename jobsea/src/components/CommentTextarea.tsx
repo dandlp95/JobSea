37f97df65fcc6ee8f035b112325741fa41ee25e2
Daniel De La Pena <del17011@byui.edu>
2023-08-27 18:35:20 -0600

    migrated Comment textarea to typescript

---
+++ jobsea/src/components/CommentTextarea.tsx
@@ -0,0 +1,24 @@
+import React, { ChangeEventHandler } from 'react'
+import commentTextareaCSS from './CommentTextarea.module.css'
+
+type Props = {
+  isReadonly: boolean,
+  comments: string,
+  handleCommentChange: ChangeEventHandler,
+  labelText: string
+}
+const CommentTextarea: React.FunctionComponent<Props> = ({ isReadonly, comments, handleCommentChange, labelText }) => {
+  return (
+    <div className={commentTextareaCSS.CommentTextarea}>
+      <label htmlFor='comment'>{labelText}</label>
+      <textarea
+        readOnly={isReadonly ? true : false}
+        name='comment'
+        value={comments}
+        onChange={handleCommentChange}
+      />
+    </div>
+  )
+}
+
+export default CommentTextarea
