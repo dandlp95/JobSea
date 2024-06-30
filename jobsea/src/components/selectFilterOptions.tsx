96661beab1c7d5dff74cb8c7f6312468c6f74347
Daniel De La Pena <del17011@byui.edu>
2024-06-30 03:55:17 -0600

    filter list box

--- jobsea/src/components/selectFilterOptions.tsx
+++ jobsea/src/components/selectFilterOptions.tsx
@@ -1,36 +1,41 @@
-import React from "react"
-import { FilterOptions } from "../customTypes/requestTypes"
-import { listFilterKeys } from "../customTypes/enumTypes"
+import React from 'react'
+import { FilterOptions } from '../customTypes/requestTypes'
+import { listFilterKeys } from '../customTypes/enumTypes'
 import selectFilterOptionsCSS from './selectFilterOptions.module.css'
 
 type Props = {
-    selectedOptions: string[]
-    updateSelectedOptions: (updatedOptions: string[], optionsType: listFilterKeys) => void
-    listType: listFilterKeys
-  }
-  /*This components will show all the selected options from a field.*/
-  const SelectFilterOptions: React.FunctionComponent<Props> = ({
-    selectedOptions,
-    updateSelectedOptions,
-    listType
-  }) => {
+  selectedOptions: string[]
+  updateSelectedOptions: (updatedOptions: string[], optionsType: listFilterKeys) => void
+  listType: listFilterKeys
+}
+/*This components will show all the selected options from a field.*/
+const SelectFilterOptions: React.FunctionComponent<Props> = ({
+  selectedOptions,
+  updateSelectedOptions,
+  listType
+}) => {
+  const handleOptionRemoval = (index: number) => {
+    selectedOptions.splice(index, 1)
 
-    const handleOptionRemoval = (index: number) => {
-      selectedOptions.splice(index, 1)
-  
-      //Sends the updated list to parent component
-      updateSelectedOptions(selectedOptions, listType)
-    }
-  
-    return (
-      <div className={selectFilterOptionsCSS.filterOptionsMainDiv}>
-        {selectedOptions.map((option, index) => (
-          <div>
-            {option}<button onClick={() => handleOptionRemoval(index)}>X</button>
-          </div>
-        ))}
-      </div>
-    )
+    //Sends the updated list to parent component
+    updateSelectedOptions(selectedOptions, listType)
   }
 
-export default SelectFilterOptions 
+  return (
+    <div className={selectFilterOptionsCSS.filterOptionsMainDiv}>
+      {selectedOptions.map((option, index) => (
+        <div>
+          {option} {' '}
+          <span
+            className={selectFilterOptionsCSS.filterItemButton}
+            onClick={() => handleOptionRemoval(index)}
+          >
+            x
+          </span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default SelectFilterOptions
